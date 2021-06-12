279d5086dd7a68777fa19f359041e21304f1b485
Sajag Shrestha <sajagshrestha0852@gmail.com>
2021-06-12 11:09:20 +0545

    addition of docs

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,12 +1,24 @@
 import jwt from "jsonwebtoken";
 
+/**
+ * create jwt token
+ *
+ * @param   {Object}   payload
+ * @returns {String}
+ */
 export const createAccessToken = (payload) => {
 	return jwt.sign(payload, process.env.SECRET_KEY);
 };
 
+/**
+ * verify jwt token
+ *
+ * @param   {String}   token
+ * @returns {Promise}
+ */
 export const verifyToken = async (token) => {
 	try {
-		var decoded = jwt.verify(token, process.env.SECRET_KEY);
+		const decoded = jwt.verify(token, process.env.SECRET_KEY);
 		return decoded;
 	} catch (err) {
 		throw err;
--- src/utils/passwordHasher.js
+++ src/utils/passwordHasher.js
@@ -1,10 +1,23 @@
 import bcrypt from "bcrypt";
 
+/**
+ * create password hash
+ *
+ * @param   {String}   password
+ * @returns {Promise}
+ */
 export const hashPassword = async (password) => {
 	const ROUNDS = 10;
 	return bcrypt.hash(password, ROUNDS);
 };
 
+/**
+ * verify hashed password
+ *
+ * @param   {String}   password
+ * @param   {String}   hashedPassword
+ * @returns {Promise}
+ */
 export const compareHashedPassword = async (password, hashedPassword) => {
 	return bcrypt.compare(password, hashedPassword);
 };
